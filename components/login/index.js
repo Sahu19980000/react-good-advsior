57e470760f2eefb6906329fb1b54283ba366d98a
Sahu19980000 <newmobilespoint@gmail.com>
2024-07-18 13:34:44 +0530

    code updated

--- components/login/index.js
+++ components/login/index.js
@@ -1,17 +1,36 @@
+"use client";
+import axios from "axios";
 import React, { useState } from "react";
 
-export default function Login() {
-  const [email, setEmail] = useState("user@gmail.com"); // Default username
-  const [password, setPassword] = useState("12345raja"); // Default password
+const LoginPanel = (props) => {
+
+  const [form,Setform ] = useState(true);
+  const[email,Setemail] = useState("user@gmail.com");
+  const[password,Setpassword] = useState("12345raja")
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
+   
+  const handleClose = () => {
+    let ans = document.getElementsByClassName("login-container")[0];
+    
+    console.log(ans);
+    ans.style.display = "none";
+  };
 
-  const base_url = "https://dish.najmainternational.com/api/user"; 
+  const show_form = (data)=>{
+   if(data == 'signup'){
+    Setform(false);
+   }
+   else{
+    Setform(true);
+   }
+    
+  } 
 
-  const handleLogin = async () => {
-
+  const get_login = async ()=>{
     try {
-      const res = await fetch(base_url+'/login',
+      const res = await fetch(
+        "https://dish.najmainternational.com/api/user/login",
         {
           method: "POST",
           headers: {
@@ -22,40 +41,130 @@ export default function Login() {
             password,
           }),
         }
-      );
-
-      // if (!res.ok) {
-      //   throw new Error(
-      //     HTTP error! status: ${res.status} - ${res.statusText}
-      //   );
-      // }
-
+      )
       const data = await res.json();
       setResponse(data);
-    } catch (err) {
+    }
+    catch (err) {
       setError(err.message);
     }
-  };
+  }
+  
 
   return (
-    <div className="App">
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Username"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <button onClick={handleLogin}>Submit</button>
-      <h1>Hello Vite + React</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      {response && <div>Response: {JSON.stringify(response)}</div>}
-      {error && <div>Error: {error}</div>}
-    </div>
+    <section className="login-container">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-8">
+            <div className="sign-left"></div>
+          </div>
+          <div className="col-lg-4">
+            <div className="sign-right p-5">
+              <div className="cross-icon text-end">
+                <button onClick={handleClose}>
+                  <img src="../../images/icon/cross-icon.png" alt="Close" />
+                </button>
+              </div>
+              {
+                form ? <div className="login-box">
+                <h5 className="heading mt-3 mb-3">Sign in to your account</h5>
+                <p className="mt-3 mb-3">
+                  Hey, Enter your details to get sign into your account{" "}
+                </p>
+
+                <form>
+                  <div className="mt-3 mb-3 p-2 login-form">
+                    <label className="text-muted">enter email id</label>
+                    <input
+                      type="text"
+                      placeholder="example@gmail.com"
+                      className="sign-email"
+                      value={email}
+                    />
+                  </div>
+                  <div className="mt-3 mb-3 p-2 login-form">
+                    <label className="text-muted">enter password </label>
+                    <input
+                      type="text"
+                      placeholder="example:1234"
+                      className="sign-password"
+                      value={password}
+                    />
+                  </div>
+                  <input type="button" value='Get Started' className="sign-btn" onClick={get_login}></input>
+                  {response && <div>Response: {JSON.stringify(response)}</div>}
+                  {error && <div>Error: {error}</div>}
+                </form>
+
+                <div className="mt-4 mb-3">
+                  <p >
+                    Don’t have an account?{" "}
+                    <a href="#" onClick={()=>show_form("signup")}>Sign up now</a>
+                  </p>
+                </div>
+              </div>:''
+              }
+
+              {
+                form ? '':<div className="signup-box">
+                <h5 className="heading mt-3 mb-3">
+                  Sigup in to your account
+                </h5>
+                <p className="mt-3 mb-3">
+                  Hey, Enter your details to get signup into your account{" "}
+                </p>
+
+                <form>
+                <div className="mt-3 mb-3 p-2 login-form">
+                    <label className="text-muted">enter full name </label>
+                    <input
+                      type="text"
+                      placeholder="enter your name"
+                      className="sign-email"
+                    />
+                </div>
+                  <div className="mt-3 mb-3 p-2 login-form">
+                    <label className="text-muted">enter email id</label>
+                    <input
+                      type="text"
+                      placeholder="example@gmail.com"
+                      className="sign-email"
+                    />
+                  </div>
+                  
+                  <div className="mt-3 mb-3 p-2 login-form">
+                    <label className="text-muted">enter phone no </label>
+                    <input
+                      type="text"
+                      placeholder="example:+91000000"
+                      className="sign-email"
+                    />
+                  </div>
+
+                  <div className="mt-3 mb-3 p-2 login-form">
+                    <label className="text-muted">enter password </label>
+                    <input
+                      type="text"
+                      placeholder="example:1234"
+                      className="sign-email"
+                    />
+                  </div>
+
+                  <button className="signup-btn">Get Started</button>
+                </form>
+
+                <div className="mt-4 mb-3">
+                  <p>please <a href="#" onClick={()=>show_form('login')}>login now</a></p>
+              </div>
+            </div>
+              }
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default LoginPanel;
